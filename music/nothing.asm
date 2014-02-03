8628b954eb052a754d377982f9a4739ff405b18b
yenatch <yenatch@gmail.com>
2014-02-03 03:07:11 -0500

    Dump the music of Pokemon Pinball (U).

---
+++ music/nothing.asm
@@ -0,0 +1,17 @@
+Music_Nothing: ; 3ccb2
+	dbw $c0, Music_Nothing_Ch1
+	dbw $01, Music_Nothing_Ch2
+	dbw $02, Music_Nothing_Ch3
+	dbw $03, Music_Nothing_Ch4
+; 3ccbe
+
+
+Music_Nothing_Ch1: ; 3ccbe
+
+Music_Nothing_Ch2: ; 3ccbe
+
+Music_Nothing_Ch3: ; 3ccbe
+
+Music_Nothing_Ch4: ; 3ccbe
+	endchannel
+; 3ccbf
